02d7f3486c39dbfd66fae2672d1fa9bb4bbfaa18
Mihailo Milosevic <mmilosevic@tenstorrent.com>
2025-12-03 09:44:05 +0100

    [TT-XLA][PyTorch] Training Workload for Llama 3.2 1B - Adapter Layers (#303)
    ### Ticket
    Closes https://github.com/tenstorrent/tt-blacksmith/issues/234
    
    ### Model Training Setting
    
    - **Model:** `Llama 3.2 1B`
    - **Strategy:** SFT with Adapter Layers on a single chip (N150)
    - **Framework:** PyTorch
    - **Task:** Sentiment prediction using `SST2`
    
    ### What's Changed
    
    This PR touches on the following things:
    - Refactored `llama` experiment to support different training methods
    (apart from LoRA).
    - Implemented `llama` with Adapter Layers training workload through
    TT-XLA.
    - Added functionality for loading model from huggingface with adapter
    layers.
    - Moved common requirements to `requirements.txt`
    
    ### Experiment Results
    
    For completeness here is a train/loss curve of the experiment runs
    during testing.
    
    <img width="5056" height="2656" alt="W B Chart 02_12_2025, 12_31_16"
    src="https://github.com/user-attachments/assets/56c53e6a-d885-4b1f-b9bf-e88a9816f535"
    />
    
    
    ### Checklist
    - [x] Main training workflow implementation
    - [x] CPU testing
    - [x] GPU testing
    - [x] TT device testing

--- blacksmith/models/torch/huggingface/hf_models.py
+++ blacksmith/models/torch/huggingface/hf_models.py
@@ -2,6 +2,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 import torch
+import torch.nn as nn
 from transformers import AutoModelForCausalLM
 from peft import LoraConfig, get_peft_model
 
@@ -16,8 +17,12 @@ def get_model(config: TrainingConfig, device: torch.device):
 
     # Apply training specific modifications
     # Apply LoRA if rank is specified
-    if config.lora_r > 0:
+    if config.training_type == "lora":
         model = _apply_lora(model, config)
+    elif config.training_type == "adapters":
+        _apply_adapters(model, config)
+    else:
+        raise ValueError(f"Invalid training type: {config.training_type}")
 
     model.to(eval(config.dtype))
     model.to(device)
@@ -34,3 +39,52 @@ def _apply_lora(model, config: TrainingConfig):
     )
 
     return get_peft_model(model, lora_config)
+
+
+def _apply_adapters(model, config: TrainingConfig):
+    # Freeze all layers
+    for param in model.parameters():
+        param.requires_grad = False
+
+    # Apply adapters
+    if len(config.adapter_layers) == 0:
+        adapter_layers = list(range(len(model.model.layers)))
+    else:
+        adapter_layers = config.adapter_layers
+
+    for block_idx in adapter_layers:
+        #### Insert first adapter
+        original_layer_output = model.model.layers[block_idx].self_attn.o_proj
+        adapted_layer = make_adapted_layer(original_layer_output, config)
+        model.model.layers[block_idx].self_attn.o_proj = adapted_layer
+
+        #### Insert second adapter
+        original_layer_output = model.model.layers[block_idx].mlp.down_proj
+        adapted_layer = make_adapted_layer(original_layer_output, config)
+        model.model.layers[block_idx].mlp.down_proj = adapted_layer
+
+    return model
+
+
+def make_adapted_layer(linear, config: TrainingConfig):
+    class ResidualAdapter(nn.Module):
+        def __init__(self, linear, bottleneck_dim):
+            super().__init__()
+            self.linear = linear
+            d = linear.out_features
+
+            self.adapter = nn.Sequential(
+                nn.Linear(d, bottleneck_dim),
+                nn.GELU(),
+                nn.Linear(bottleneck_dim, d),
+            )
+
+            # Start as identity
+            nn.init.zeros_(self.adapter[-1].weight)
+            nn.init.zeros_(self.adapter[-1].bias)
+
+        def forward(self, x):
+            y = self.linear(x)
+            return y + self.adapter(y)
+
+    return ResidualAdapter(linear, config.adapter_bottleneck_dim)
